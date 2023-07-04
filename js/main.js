7868210e90270f3345c63da5213306097c75aeaa
Waldir Marquez POD <waldir.marquez@hirewithpod.com>
2023-07-04 01:24:01 -0500

    west start first version

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+// Show menu
+menuBtn.addEventListener('click', ()=> {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+// Hide menu
+closeBtn.addEventListener('click', ()=> {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+const navItems = menu.querySelectorAll('li');
+
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+}
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', ()=> {
+        changeActiveItem();
+        link.classList.add('active')
+    })
+})
+
+// show/hide skills items
+const skillItems = document.querySelectorAll('section.skills .skill');
+
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.item').classList.toggle('show-items');
+    })
+})
+
+// add box shadow on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+})
